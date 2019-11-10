341d26c29eb6e00f04d73414a275694343721533
LovepreetSingh-09 <54183912+LovepreetSingh-09@users.noreply.github.com>
2019-11-10 23:00:43 +0530

    Update model_selection.R

--- model_selection.R
+++ model_selection.R
@@ -71,3 +71,48 @@ for(j in 1:k){
      cv.errors [j,i]=mean((hitters$Salary[folds ==j]-pred)^2)
      }
    }
+
+# Ridge Regression
+# alpha=0 for ridge and 1 for lasso
+library(glmnet)
+x=model.matrix(Salary~.,hitters)[,-1]
+x
+y=hitters$Salary
+y
+grid=10^seq(10,-2,length=100)
+grid
+# glmnet automatically standardize the variables
+?glmnet
+ridge=glmnet(x,y,alpha=0,lambda=grid)
+dim(coef(ridge))
+ridge$lambda[50]
+coef(ridge)[,50]
+sqrt(sum(coef(ridge)[-1,50]^2))
+predict(ridge,s=50,type='coefficients')
+
+set.seed (1)
+train=sample (1: nrow(x), nrow(x)/2)
+test=(- train )
+y.test=y[test]
+ridge=glmnet(x[train,],y[train],lambda=grid,alpha=0,thresh = 1e-12)
+pred=predict(ridge,s=5,newx=x[test,])
+mean((y[test]-pred)^2)
+
+# glmnet() yield the exact least squares coefficients when λ = 0, for this we use the argument exact=T
+pred=predict(ridge,s=0, newx=x[test,], exact=TRUE)
+
+lm(y∼x, subset =train)
+predict(ridge ,s=0, exact =T,type= 'coefficients')
+
+library (pls)
+set.seed (2)
+pcr=pcr(Salary∼., data=Hitters ,scale=TRUE , validation ="CV")
+summary(pcr)
+validationplot(pcr ,val.type= 'MSEP')
+
+set.seed (1)
+pls=plsr(Salary∼., data=Hitters ,subset =train ,scale=TRUE ,validation ="CV")
+summary(pls)
+validationplot(pls ,val.type="MSEP")
+pls.pred=predict (pls ,x[test ,], ncomp =2)
+mean((pls.pred -y.test)^2)
